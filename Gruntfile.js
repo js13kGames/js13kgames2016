a95a0435d98ff9dfbbf39e5b284e8ec923bf4805
Kev Adsett <kev.adsett@gmail.com>
2016-08-14 18:03:51 +0100

    Setup work flow to produce minified js and css

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,13 +8,35 @@ module.exports = function(grunt) {
         src : 'src/**/*.js',
         dest : 'build/output.min.js'
       }
+    },
+    less: {
+      development: {
+        files: {
+          'src/css/style.css': 'src/css/style.less'
+        }
+      },
+      production: {
+        files: {
+          'build/css/output.css': 'src/css/style.less'
+        }
+      }
+    },
+    cssmin: {
+      target: {
+        files: {
+          'build/css/output.css': 'build/css/output.css'
+        }
+      }
     }
   });
 
-  // Load the plugin that provides the "uglify" task.
+  // Load the plugins
   grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-less');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
 
   // Default task(s).
-  grunt.registerTask('default', ['uglify']);
+  grunt.registerTask('default', ['less:development']);
+  grunt.registerTask('production', ['uglify', 'less:production', 'cssmin']);
 
 };
