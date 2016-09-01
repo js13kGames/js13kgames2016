463b8351f8a5ddba4151ba8079c84b7aa7b22b73
Kev Adsett <kev.adsett@gmail.com>
2016-09-01 14:35:08 +0100

    Rename dotlines to crosslines.

--- src/js/models/LineColliderModel.js
+++ src/js/models/LineColliderModel.js
@@ -11,14 +11,14 @@ LineColliderModel.prototype = {
 		var thisLine = this.b.subtract(this.a);
 		var otherLine = otherB.subtract(otherA);
 
-		var dotLines = otherLine.x * thisLine.y - otherLine.y * thisLine.x;
+		var crossLines = otherLine.x * thisLine.y - otherLine.y * thisLine.x;
 
-		if (dotLines === 0) return;
+		if (crossLines === 0) return;
 
-		var check1 = ( deltaA.x * otherLine.y - deltaA.y * otherLine.x ) / dotLines;
+		var check1 = ( deltaA.x * otherLine.y - deltaA.y * otherLine.x ) / crossLines;
 		if( check1 < 0 || check1 > 1 ) return;
 
-		var check2 = ( deltaA.x * thisLine.y - deltaA.y * thisLine.x ) / dotLines;
+		var check2 = ( deltaA.x * thisLine.y - deltaA.y * thisLine.x ) / crossLines;
 		if( check2 < 0 || check2 > 1 ) return;
 
 		return {
