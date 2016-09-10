8dc01325e6cbf10dbd6b41dbfb23a93bdc02ce99
Kev Adsett <kev.adsett@gmail.com>
2016-09-10 15:08:28 +0100

    Revert to older, simpler LineCollider generation.

--- src/js/controllers/LevelCollisionController.js
+++ src/js/controllers/LevelCollisionController.js
@@ -14,112 +14,57 @@ LevelCollisionController.prototype = {
 		var width = TILE_SIZE;
 		var height = TILE_SIZE;
 
-		for (r = 0; r < data.length - 1; r++) {
-			for (c = 0; c < data[r].length - 1; c++) {
-				var A = data[r][c],
-					B = data[r][c+1],
-					C = data[r+1][c],
-					D = data[r+1][c+1];
-				if (A == B && A == C && A == D) continue;
-				var isTopLeftCorner = B === C && D === 0 && B !== 0;
-				var isBottomRightCorner = B == C && A === 0 && B !== 0;
-				var isTopRightCorner = A === D && C === 0 && A !== 0;
-				var isBottomLeftCorner = A === D && B === 0 && A !== 0;
-				var isCorner = isBottomLeftCorner || isTopRightCorner || isBottomRightCorner || isTopLeftCorner;
-
-				var isTopEdge = A !== C && A !== 0;
-				var isBottomEdge = C !== A && C !== 0;
-				var isLeftEdge = A !== B && A !== 0;
-				var isRightEdge = B !== A && B !== 0;
-				if (isBottomLeftCorner) {
-					this.setLineCollider(r, c + 1, "left",
-						new Vector2(width * (c + 1), height * (r + 1) + this.OVERLAP),
-						new Vector2(width * (c + 1), height * r),
-						A
-					);
-
-					this.setLineCollider(r, c + 1, "bottom",
-						new Vector2(width * (c + 2), (height * (r+1))),
-						new Vector2(width * (c + 1) - this.OVERLAP, (height * (r+1))),
-						A
-					);
+		for (r = 0; r < data.length; r++) {
+			prevCell = undefined;
+			for (c = 0; c < data[r].length; c++) {
+				currentCell = data[r][c];
+				if (prevCell != undefined) {
+					if (prevCell != currentCell) {
+						if (currentCell !== 0) {
+							this.collisions.push(new LineColliderController(
+								new Vector2(width * c, height * r),
+								new Vector2(width * c, height * (r+1)),
+								currentCell
+							));
+						}
+						if (prevCell !== 0) {
+							this.collisions.push(new LineColliderController(
+								new Vector2(width * c, height * (r+1)),
+								new Vector2(width * c, height * r),
+								prevCell
+							));
+						}
+						
+					}
 				}
-				if (isTopRightCorner) {
-					this.setLineCollider(r + 1, c, "right",
-						new Vector2(width * (c + 1), (height * (r+1)) - this.OVERLAP),
-						new Vector2(width * (c + 1), height * (r + 2)),
-						A
-					);
-
-					this.setLineCollider(r + 1, c, "top",
-						new Vector2(width * c, (height * (r+1))),
-						new Vector2(width * (c + 1) + this.OVERLAP, (height * (r+1))),
-						A
-					);
-				}
-				if (isBottomRightCorner) {
-					this.setLineCollider(r, c, "right",
-						new Vector2(width * (c + 1), height * r),
-						new Vector2(width * (c + 1), height * (r + 1) + this.OVERLAP),
-						A
-					);
-
-					this.setLineCollider(r, c, "bottom",
-						new Vector2(width * (c + 1) + this.OVERLAP, (height * (r+1))),
-						new Vector2(width * c, (height * (r+1))),
-						A
-					);
-				}
-				if (isTopLeftCorner) {
-					this.setLineCollider(r + 1, c + 1, "left",
-						new Vector2(width * (c + 1), height * (r + 2)),
-						new Vector2(width * (c + 1), (height * (r+1)) - this.OVERLAP),
-						A
-					);
-					
-					this.setLineCollider(r + 1, c + 1, "top",
-						new Vector2(width * (c + 1) - this.OVERLAP, (height * (r+1))),
-						new Vector2(width * (c + 2), (height * (r+1))),
-						A
-					);
-				}
-
-				if (isTopEdge) {
-					this.setLineCollider(r + 1, c, "top",
-						new Vector2(width * (c), height * (r + 1)),
-						new Vector2(width * (c + 1), height * (r + 1)),
-						A
-					);
-				}
-				if (isBottomEdge) {
-					this.setLineCollider(r + 1, c, "bottom",
-						new Vector2(width * (c + 1), height * (r + 1)),
-						new Vector2(width * (c), height * (r + 1)),
-						C
-					);
-				}
-				if (isLeftEdge) {
-					this.setLineCollider(r + 1, c, "left",
-						new Vector2(width * (c + 1), height * (r + 1)),
-						new Vector2(width * (c + 1), height * (r)),
-						A
-					);
-				}
-
-				if (isRightEdge) {
-					this.setLineCollider(r + 1, c, "right",
-						new Vector2(width * (c + 1), height * (r)),
-						new Vector2(width * (c + 1), height * (r + 1)),
-						B
-					);
-				}
+				prevCell = currentCell;
 			}
 		}
 
-	},
-	setLineCollider: function(row, col, edge, pointA, pointB, type) {
-		lineCollider = new LineColliderController (pointA, pointB, type);
-		this.collisions.push(lineCollider);
+		for (c = 0; c < data[0].length; c++) {
+			for (r = 0; r < data.length; r++) {
+				currentCell = data[r][c];
+				if (prevCell != undefined) {
+					if (prevCell != currentCell) {
+						if (currentCell !== 0) {
+							this.collisions.push(new LineColliderController(
+								new Vector2(width * (c+1), height * r),
+								new Vector2(width * c, height * r),
+								currentCell
+							));
+						}
+						if (prevCell !== 0) {
+							this.collisions.push(new LineColliderController(
+								new Vector2(width * c, height * r),
+								new Vector2(width * (c+1), width * r),
+								prevCell
+							));
+						}
+					}
+				}
+				prevCell = currentCell;
+			}
+		}
 	},
 	destroyCollisionData: function() {
 		for (var i = 0; i < this.collisions.length; i++) {
