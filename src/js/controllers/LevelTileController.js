614b3c92439ec5f19afc76165ee082075d88a2e0
Kev Adsett <kev.adsett@gmail.com>
2016-09-10 17:50:56 +0100

    Start on a more "glitchy" effect.

--- src/js/controllers/LevelTileController.js
+++ src/js/controllers/LevelTileController.js
@@ -2,6 +2,7 @@ function LevelTileController(position, width, height, type, index) {
 	this.model = new LevelTileModel(position, width, height, type, index);
 	this.view = new LevelTileView(this.model);
 	GameEvents.on("glitchModeChanged", this.onGlitchModeChanged, this);
+	GameEvents.on("update", this.update, this);
 	this.onGlitchModeChanged(window.glitchMode);
 }
 
@@ -12,8 +13,40 @@ LevelTileController.prototype = {
 	onGlitchModeChanged: function(glitchMode) {
 		this.model.isGlitching = this.model.type.name === glitchMode;
 	},
+	update: function(dt) {
+		if (this.model.isGlitching) {
+			if (!this.model.isShowingGlitch) {
+				if (Math.random() > 0.99) {
+					this.model.isShowingGlitch = true;
+					this.model.glitchStartTime = Date.now();
+					this.generateGlitchParameters();
+				}
+			} else {
+				if (Math.random() > 0.99) {
+					this.view.changeGlitchDisplay();
+				}
+				if (Date.now() - this.model.glitchStartTime > this.model.glitchTime) {
+					this.model.isShowingGlitch = false;
+					this.view.resetGlitchDisplay();
+				}
+			}
+		}
+	},
+	generateGlitchParameters: function() {
+		this.model.glitchTime = Math.random() * 2000;
+		this.model.canvasGrabSize = this.generateRandomSizeVector();
+		this.model.canvasGrabSourcePosition = this.generateRandomSizeVector();
+		this.model.canvasGrabDestPosition = this.generateRandomSizeVector();
+	},
+	generateRandomSizeVector: function() {
+		return new Vector2(
+			((Math.random() * TILE_SIZE) - TILE_SIZE/2) * window.gameScale,
+			((Math.random() * TILE_SIZE) - TILE_SIZE/2) * window.gameScale
+		);
+	},
 	destroy: function() {
 		this.view.destroy();
 		GameEvents.off("glitchModeChanged", this.onGlitchModeChanged, this);
+		GameEvents.off("update", this.update, this);
 	}
 }
--- src/js/views/LevelTileView.js
+++ src/js/views/LevelTileView.js
@@ -1,6 +1,7 @@
 function LevelTileView(model) {
 	this.model = model;
 	GameEvents.on('draw', this.draw, this);
+	this.glitchOffsets = [new Vector2(0, 0), new Vector2(0, 0), new Vector2(0, 0)];
 }
 
 LevelTileView.prototype = {
@@ -9,17 +10,20 @@ LevelTileView.prototype = {
 		if (layer === "level" && this.model.type.name === "blocker") return;
 		if (layer === "background" && this.model.type.name !== "blocker") return;
 
-		var scaledPosition = this.model.position.scale(window.gameScale);
 
-		if (this.model.isGlitching) {
-			ctx.globalAlpha = 0.1;
+		if (this.model.type.name === "floor" || this.model.type.name === "antifloor") {
+			if (this.model.isGlitching) {
+				ctx.globalCompositeOperation = "screen";
+				this.drawRect("cyan", 1/8, this.glitchOffsets[0], this.model.isShowingGlitch);
+				this.drawRect("magenta", 1/8, this.glitchOffsets[1], this.model.isShowingGlitch);
+				this.drawRect("yellow", 1/8, this.glitchOffsets[2]), this.model.isShowingGlitch;
+				
+			} else {
+				this.drawRect("white", 1);
+			}
 		} else {
-			ctx.globalAlpha = 1;
+			this.drawRect(this.model.type.colour, 1);
 		}
-
-		var scaledSize = TILE_SIZE * window.gameScale;
-		ctx.fillStyle = this.model.type.colour;
-		ctx.fillRect(scaledPosition.x, scaledPosition.y, scaledSize, scaledSize);
 
 		if (window.drawDebug) {
 			ctx.fillStyle = "white";
@@ -27,6 +31,44 @@ LevelTileView.prototype = {
 			ctx.textAlign = "left";
 			ctx.fillText(this.model.index, scaledPosition.x + 6, scaledPosition.y + 18);
 		}
+		ctx.globalCompositeOperation = "source-over";
+	},
+	changeGlitchDisplay: function() {
+		this.glitchOffsets[0].x = (Math.random() * 20) - 10;
+		this.glitchOffsets[0].y = (Math.random() * 20) - 10;
+		this.glitchOffsets[1].x = (Math.random() * 20) - 10;
+		this.glitchOffsets[1].y = (Math.random() * 20) - 10;
+		this.glitchOffsets[2].x = (Math.random() * 20) - 10;
+		this.glitchOffsets[2].y = (Math.random() * 20) - 10;
+	},
+	resetGlitchDisplay: function() {
+		this.glitchOffsets = [new Vector2(0, 0), new Vector2(0, 0), new Vector2(0, 0)];
+	},
+	drawRect: function(colour, alpha, offset, randomiseCanvas) {
+		if (!offset) {
+			offset = new Vector2(0, 0);
+		}
+		var scaledPosition = this.model.position.scale(window.gameScale).add(offset);
+		var scaledSize = TILE_SIZE * window.gameScale;
+		if (randomiseCanvas) {
+			ctx.drawImage(
+				ctx.canvas,
+				scaledPosition.x + this.model.canvasGrabSourcePosition.x,
+				scaledPosition.y + this.model.canvasGrabSourcePosition.y,
+				scaledSize + this.model.canvasGrabSize.x,
+				scaledSize + this.model.canvasGrabSize.y,
+				scaledPosition.x + this.model.canvasGrabDestPosition.x,
+				scaledPosition.y + this.model.canvasGrabDestPosition.y,
+				scaledSize + this.model.canvasGrabSize.x,
+				scaledSize + this.model.canvasGrabSize.y
+			);
+		}
+		ctx.globalAlpha = alpha;
+		ctx.fillStyle = colour;
+		ctx.fillRect(scaledPosition.x, scaledPosition.y, scaledSize, scaledSize);
+
+		
+
 	},
 	destroy: function() {
 		GameEvents.off('draw', this.draw, this);
