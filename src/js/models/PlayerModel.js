a3ecdd0550d1d35bcc54601046f129c702985d8b
Kev Adsett <kev.adsett@gmail.com>
2016-09-10 21:59:18 +0100

    Start player with a little right-wards velocity.

--- src/js/models/PlayerModel.js
+++ src/js/models/PlayerModel.js
@@ -8,7 +8,7 @@ PlayerModel.prototype = {
 		this.direction = 0;
 		this.isOnFloor = false;
 		this.size = PLAYER_SIZE;
-		this.velocity = new Vector2(0, 0);
+		this.velocity = new Vector2(100, 0);
 		this.position = new Vector2(0, 0);
 		this.jumpCount = MAX_JUMPS;
 	}
